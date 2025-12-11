e0676a9f6741c8c506ddc0d983ec11915e4bcc67
rajuu8 <r2499178@gmail.com>
2025-12-11 12:38:20 +0545

    unlimited img gentrear

--- style.js
+++ style.js
@@ -1,54 +1,53 @@
 let btn = document.querySelector('button');
-let main = document.querySelector('main')
+let main = document.querySelector('main');
+let h1 = document.querySelector('h1');
 
-// btn.addEventListener('click',()=> {
+let arr = {
+    models: [
+        "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?w=900",
+        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=900",
+        "https://images.unsplash.com/photo-1521190339977-0e75f2d2d258?w=900",
+        "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?w=900",
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=900",
+        "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=900",
+        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=900",
+        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=900",
+        "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?w=900",
+        "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?w=900",
+        "https://images.unsplash.com/photo-1524503033411-c9566986fc8f?w=900",
+        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=900",
+        "https://images.unsplash.com/photo-1428930377079-45a584b6dd6b?w=900",
+        "https://images.unsplash.com/photo-1517841905240-472988babdf9?w=900",
+        "https://images.unsplash.com/photo-1496346556741-1e6ec06fe944?w=900",
+        "https://images.unsplash.com/photo-1521119989659-a83eee488004?w=900"
+    ]
+};
 
-//     let x = Math.floor(Math.random()*100)
-//     let y = Math.floor(Math.random()*100)
+main.addEventListener('mousemove', () => {
 
+    let img = document.createElement('img');
 
-//     let c1 = Math.floor(Math.random()*256)
-//     let c2 = Math.floor(Math.random()*256)
-//     let c3 = Math.floor(Math.random()*256)
+    // Correct way to get random model image
+    let index = Math.floor(Math.random() * arr.models.length);
 
-//     let colors = `rgb(${c1} , ${c2} ,${c3})`
-    
-//     let div = document.createElement('div')
-//     div.style.height = '50px'
-//     div.style.width = '50px'
-//     div.style.position = 'absolute'
-//     div.style.backgroundColor = colors
+    // Random position
+    let x = Math.floor(Math.random() * 80);
+    let y = Math.floor(Math.random() * 80);
 
+    // Rotate
+    let r = Math.floor(Math.random() * 360);
 
-//     div.style.left = x + '%'
-//     div.style.top = y + '%'
+    // Scale
+    let s = Math.random() * 1.5 + 0.5; // 0.5 - 2 scale
 
+    img.src = arr.models[index];
+    img.style.width = "50px";
+    img.style.position = "absolute";
+    img.style.top = x + "%";
+    img.style.left = y + "%";
+    img.style.rotate = r + "deg";
+    img.style.transform = `scale(${s})`;
+    img.style.borderRadius = "50%";
 
-//     main.appendChild(div)
-// })
-
-
-btn.addEventListener('click' , ()=>{
-    let div = document.createElement('div')    
-
-    let x = Math.random()*90
-    let y = Math.random()*90
-
-    let c1 = Math.floor(Math.random()*256)
-    let c2 = Math.floor(Math.random()*256)
-    let c3 = Math.floor(Math.random()*256)
-
-
-    div.style.height = '50px'
-    div.style.width = '50px'
-    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-    div.style.position = 'absolute'
-    div.style.top = x + '%'
-    div.style.left = y + '%'
-
-
-
-
-    main.appendChild(div)
-    
-})
+    main.appendChild(img);
+});
