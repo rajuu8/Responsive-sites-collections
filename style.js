bf7651965d11d95fdcdf855565d13f5e8d53cb55
rajuu8 <r2499178@gmail.com>
2025-12-13 20:06:28 +0545

    add frnd remove frnd anamation created in cohort-2

--- style.js
+++ style.js
@@ -1,17 +1,16 @@
-let h1 = document.querySelector('h1');
+let btnall = document.querySelectorAll('button');
 
-let aud = new Audio('./39.mp3');
-let aud1 = new Audio('./40.mp3');
+btnall.forEach((elem)=> {
+    elem.addEventListener('click' ,()=> {
 
-document.addEventListener('keydown', (e) => {
-    h1.innerHTML = e.code;
 
-    if (e.code === 'KeyA') {
-        aud.currentTime = 0;
-        aud.play();
-    } 
-    else if (e.code === 'KeyB') {
-        aud1.currentTime = 0;
-        aud1.play();
+    if(elem.innerHTML == 'Add frnd') {
+        elem.innerHTML = 'Remove frnd'
+
+    } else{
+        elem.innerHTML = 'Add frnd'
+
     }
-});
+    })
+    
+})
